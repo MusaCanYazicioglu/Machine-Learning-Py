1c63f4806f02084b9ec04506512812baa20c7b51
Musa Can <musacanyazicioglu@gmail.com>
2019-04-12 19:45:02 +0300

    Data Preprocessing 1.0

---
+++ Data Preprocessing/data_preprocessing_template.py
@@ -0,0 +1,30 @@
+# Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 3].values
+
+# Taking care of missing data
+from sklearn.impute import SimpleImputer
+simpleImputer = SimpleImputer(missing_values=np.nan,strategy='mean')
+simpleImputer = simpleImputer.fit(X[:,1:3])
+X[:,1:3] = simpleImputer.transform(X[:,1:3])
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,0] = labelencoder_X.fit_transform(X[:,0])
+oneHotEncoder = OneHotEncoder(categorical_features=[0])
+X = oneHotEncoder.fit_transform(X).toarray()
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(Y)
+
+# Siplitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
