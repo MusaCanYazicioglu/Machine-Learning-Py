d35d3f93e7325ae474fb3f96dbcac8519863c0fa
Musa Can <musacanyazicioglu@gmail.com>
2019-07-05 14:14:31 +0300

    Random Forest Tree

--- 2-Regression/6-Random Forest Regression/random_forest_regression.py
+++ 2-Regression/6-Random Forest Regression/random_forest_regression.py
@@ -1,4 +1,4 @@
-# Regression Template
+# Random Forest Regression
 
 # Importing the libraries
 import numpy as np
@@ -22,26 +22,20 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
-# Fitting the Regression Model to the dataset
-# Create your regressor here
+# Fitting the Random Forest Regression to the dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 300, random_state = 0)
+regressor.fit(X, y)
 
 # Predicting a new result
-y_pred = regressor.predict(6.5)
+y_pred = regressor.predict([[6.5]])
 
-# Visualising the Regression results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, regressor.predict(X), color = 'blue')
-plt.title('Truth or Bluff (Regression Model)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
-
-# Visualising the Regression results (for higher resolution and smoother curve)
-X_grid = np.arange(min(X), max(X), 0.1)
+# Visualising the Random Forest Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Regression Model)')
+plt.title('Truth or Bluff (Random Forest)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
